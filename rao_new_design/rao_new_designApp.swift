31fc7c9540921786cab4fb0eeeed5af5ed82fe41
CoMatu <30466272+CoMatu@users.noreply.github.com>
2023-03-17 13:33:15 +0300

    Initial Commit

---
+++ rao_new_design/rao_new_designApp.swift
@@ -0,0 +1,17 @@
+//
+//  rao_new_designApp.swift
+//  rao_new_design
+//
+//  Created by Вячеслав Матусевич on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct rao_new_designApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
